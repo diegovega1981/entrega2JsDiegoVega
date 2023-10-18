68352061903a0f213994ba6eb0fbdb28e52ff0fa
diegovega1981 <diegovega1981@gmail.com>
2023-10-18 11:35:33 -0300

    2da entrega módulo js
    2da entrega módulo js Diego Vega

---
+++ js/products.js
@@ -0,0 +1,34 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "banana",
+        precio: 600,
+        /*img: "https://greenshop.ar/wp-content/uploads/sites/9/2016/08/A.2.6-500GR-600x600.jpg",*/
+        img: "./images/banana.png",
+        cantidad: 1,
+    },
+
+    {
+        id: 2,
+        nombre: "pera",
+        precio: 500,
+        img: "./images/pera.jpg",
+        cantidad: 1,
+    },
+
+    {
+        id: 3,
+        nombre: "manzana verde",
+        precio: 800,
+        img: "./images/manzana verde.jpg",
+        cantidad: 1,
+    },
+
+    {
+        id: 4,
+        nombre: "manzana roja",
+        precio: 700,
+        img: "./images/manzana roja.jpg",
+        cantidad: 1,
+    },
+];
---
+++ js/script.js
@@ -0,0 +1,45 @@
+const shopContent = document.getElementById("ShopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
+const cantidadCarrito = document.getElementById("cantidadCarrito");
+
+let carrito = [];
+productos.forEach((product) => {
+  let content = document.createElement("div");
+  content.className = "card";
+  content.innerHTML = ` 
+    <img src="${product.img}">
+    <h3>${product.nombre}</h3>
+    <p class="price">$${product.precio}</p>
+    `;
+
+  shopContent.append(content);
+
+  let comprar = document.createElement("button");
+  comprar.innerText = "comprar";
+  comprar.className = "comprar";
+  content.append(comprar);
+
+  comprar.addEventListener("click", () => {
+    const repeat = carrito.some(
+      (repeatProduct) => repeatProduct.id === product.id
+    );
+    if (repeat) {
+      carrito.map((prod) => {
+        if (prod.id === product.id) {
+          prod.cantidad++;
+        }
+      });
+    } else {
+      carrito.push({
+        id: product.id,
+        nombre: product.nombre,
+        img: product.img,
+        precio: product.precio,
+        cantidad: product.cantidad,
+      });
+    }
+    console.log(carrito);
+    carritoCounter();
+  });
+});
